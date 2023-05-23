7e6c2ec4e5bf4eade484389a880a5ce4baef9e80
simonescoca <simone.scoca@icloud.com>
2023-05-23 16:09:25 +0200

    added entire code

--- script.js
+++ script.js
@@ -8,3 +8,51 @@
 //     con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe
 //     con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe
 //     con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe
+
+const ulElement = document.getElementById ("ul")
+const button = document.getElementById ("button")
+
+// FUNCTIONS ////////////////////////////////////////////
+
+/**
+ * Creates a li within an index, adds eventual classes
+ * @param index the index
+ * @param liStyle the main class
+ * @param liToggleClass the toggle class
+ * @returns the li element with upper features
+ */
+
+function createLiElement (index, liStyle, liToggleClass) {
+    const liElement = document.createElement ("li")
+    liElement.append (index)
+    liElement.classList.add (liStyle)
+    liElement.classList.toggle (liToggleClass)
+    return liElement
+}
+
+function generateGrid () {
+    while (ulElement.firstChild) {
+        ulElement.removeChild (ulElement.firstChild);
+    }
+
+    for (let i = 0 ; i < 100 ; i++) {
+        const finalLiElement = createLiElement ((i + 1), "cell", "selected")
+        ulElement.appendChild (finalLiElement)
+
+        finalLiElement.addEventListener ("click",
+            function () {
+                const selectCell = i + 1
+                console.log ("Cella selezionata: " + selectCell)
+            }
+        )
+    }
+}
+
+// EVENT LISTENERS ///////////////////////////////////////
+
+button.addEventListener ("click", 
+    function () {
+        ulElement.classList.add ("my_ul")
+        generateGrid()
+    }
+)
